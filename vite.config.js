4f6eb60be501fceef0b46b1e92cc2a617a98517d
Leileisme <onlymymind1108@gmail.com>
2025-03-14 03:16:39 +0800

    feat: 部署 github page 文件

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:process.env.NODE_ENV === 'production' ? '/React-2024-week8/' : '/',
   plugins: [react()],
 })
